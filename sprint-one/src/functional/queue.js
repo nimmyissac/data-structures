ff0b55bbe0fb8b9689523c1227810b73e82abfec
nimmyissac <nimmyissac2014@gmail.com>
2016-11-02 16:01:19 -0700

    functional complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,23 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var queueStart = 0;
+  var queueEnd = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[queueStart++] = value;
   };
 
   someInstance.dequeue = function() {
+    var temp = storage[queueEnd];
+    delete storage[queueEnd++];
+    return temp;
   };
 
   someInstance.size = function() {
+    return  (queueStart-queueEnd > 0) ? queueStart-queueEnd : 0;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,31 @@
 var Stack = function() {
   var someInstance = {};
+  // properties  in someinstance ...> storage, and size
+  // pushing means return storage[size] increment size
+  //poping means delete someinstance.storage[size]; size --;
 
-  // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {}; // va
+  var size = 0; 
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // var stackKeys = Object.keys(storage);
+    // var length = stackKeys.length;
+    // storage[length] = value;
+    storage[++size] = value;
   };
 
   someInstance.pop = function() {
+    return size > 0 ? storage[size--] : 0;
   };
 
   someInstance.size = function() {
+    return size;
+   //return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+
+//var s1 = Stack();
+//s1.push(item1);
