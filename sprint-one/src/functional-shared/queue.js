6d663bacd95ee669915f293a6ce8eda966b7ced3
nimmyissac <nimmyissac2014@gmail.com>
2016-11-02 18:50:01 -0700

    need to refactor in order to not rely on prototype linked methods

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,26 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var theQueue = Object.create(queueMethods); // var size, var storage 
+                                              // return 
+  theQueue.storage = {}; 
+  theQueue.queueEnd = 0; 
+  theQueue.queueStart = 0;
+
+  return theQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.queueStart++] = value;
+  },
 
+  dequeue: function() {
+    var temp = this.storage[this.queueEnd];
+    delete this.storage[this.queueEnd++];
+    return temp;
+  },
+
+  size: function() {
+    return  (this.queueStart-this.queueEnd > 0) ? this.queueStart-this.queueEnd : 0;
+  }
+};
 
